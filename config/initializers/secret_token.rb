75606879737a6afb14033b3db480a9090aa481ce
Derrick Mar <derrickmar1215@berkeley.edu>
2014-03-27 23:00:14 -0700

    add user and facebook authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calbeat::Application.config.secret_key_base = 'b5094abb9df4046db5120873c5cf83f5e25cb2134667679ef5551901a96d48bfd0afc64606b29b8fe0c39c2237d07dd10c72748c9dfa58f2e6be0d2ef4d9959f'
